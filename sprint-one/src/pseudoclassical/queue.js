74f041897f7e6c4938bf07f96b5d85b26ce00db8
aliotta <aliotta343@gmail.com>
2015-08-19 16:22:12 -0700

    Completed pseudoclassical pattern for stack and queue.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.min = 0;
 };
+
+Queue.prototype.enqueue = function(value){
+  var length = this.size(this.storage);
+  this.storage[length + this.min] = value;
+}
+Queue.prototype.dequeue = function(){
+  var length = this.size(this.storage);
+  var tempStore = this.storage[this.min];
+  delete this.storage[this.min];
+  this.min++;
+  return tempStore;
+}
+Queue.prototype.size = function(){
+  var length = 0;
+  for (var key in this.storage){
+    if ( this.storage.hasOwnProperty(key)){
+      length++;
+    }
+  }
+  return length;
+}
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
+
+Stack.prototype.push = function(value){
+  var length = this.size(this.storage);
+  this.storage[length] = value;
+}
+Stack.prototype.pop = function(){
+  var length = this.size(this.storage);
+  var tempStore = this.storage[length-1];
+  delete this.storage[length-1];
+  return tempStore;
+}
+Stack.prototype.size = function(){
+  var length = 0;
+  for (var key in this.storage){
+    if ( this.storage.hasOwnProperty(key)){
+      length++;
+    }
+  }
+  return length;
+}
 
 
