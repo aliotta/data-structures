95d515026481dfa92e04cda3a092d884ae317819
aliotta <aliotta343@gmail.com>
2015-08-19 16:21:04 -0700

    Completed prototypal pattern for stack and queue.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.min = 0;
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value){
+    var length = this.size(this.storage);
+    this.storage[length + this.min] = value;
+  },
+  dequeue : function(){
+    var length = this.size(this.storage);
+    var tempStore = this.storage[this.min];
+    delete this.storage[this.min];
+    this.min++;
+
+    return tempStore;
+  },
+  size : function(){
+    var length = 0;
+    for (var key in this.storage) {
+      if (this.storage.hasOwnProperty(key)) {
+        length++;
+      }
+    }
+
+    return length;    
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push : function(value) {
+    var length = this.size(this.storage);
+    this.storage[length] = value;
+  },
+  pop : function() {
+    var length = this.size(this.storage);
+    var tempStore = this.storage[length-1];
+    delete this.storage[length-1];
+
+    return tempStore;
+  },
+  size : function() {
+    var length = 0;
+    for (var key in this.storage) {
+      if (this.storage.hasOwnProperty(key)) {
+        length++;
+      }
+    }
+
+    return length;
+  }
+};
 
 
