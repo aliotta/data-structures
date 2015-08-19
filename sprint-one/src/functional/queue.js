a6df0c46b35f558f7a99b3dba5bfc73acb5d99d0
aliotta <aliotta343@gmail.com>
2015-08-19 15:17:42 -0700

    Completed standard functional queue and stack.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,15 +5,34 @@ var Queue = function(){
   var storage = {};
 
   // Implement the methods below
+  var min = 0;
 
   someInstance.enqueue = function(value){
+    var length = someInstance.size(storage);
+    storage[length+min] = value;
   };
 
   someInstance.dequeue = function(){
+    var tempStore = storage[min];
+    delete storage[min];
+    min++;
+
+    return tempStore;
   };
 
   someInstance.size = function(){
+    var length=0;
+    for (var key in storage) {
+      if (storage.hasOwnProperty(key)) {
+        length++;
+      }
+    }
+
+    return length;
   };
 
   return someInstance;
 };
+
+
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,27 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    var length = someInstance.size(storage);
+    storage[length] = value;
   };
 
   someInstance.pop = function(){
+    var length = someInstance.size(storage);
+    var tempStore = storage[length-1];
+    delete storage[length-1];
+
+    return tempStore;
   };
 
   someInstance.size = function(){
+    var length=0;
+    for (var key in storage) {
+      if (storage.hasOwnProperty(key)) {
+        length++;
+      }
+    }
+
+    return length;
   };
 
   return someInstance;
