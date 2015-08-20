9f0cb32a6f2d0aac1a7971180441a6081568d7f3
aliotta <aliotta343@gmail.com>
2015-08-20 10:26:10 -0700

    "Finished LinkList"

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if (list.tail === null) {
+      list.head = new Node(value)
+      list.tail = list.head;
+    } else {
+    list.tail.next = new Node(value);
+    list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    var tempStore = list.head.value;
+    list.head = list.head.next;
+
+    return tempStore;
   };
 
   list.contains = function(target){
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if (target === currentNode.value) {
+        return true;
+      }
+      debugger;
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
