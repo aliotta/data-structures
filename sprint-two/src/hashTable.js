c39c7ebf9cec8cd605750f06d644f3521e19e1db
aliotta <aliotta343@gmail.com>
2015-08-21 11:53:11 -0700

    Completed basics requirements for HashTables.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,19 +5,55 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var tuple = [k,v];
+  var flag = false;
+  if (!(this._storage[i])){
+    this._storage[i] = [];
+  }
+  var bucket = this._storage[i];
+  for(var j = 0; j < bucket.length; j++){
+    if(bucket[j][0] === k ){
+      bucket[j] = tuple;
+      flag = true;
+      //TODO return value
+    } 
+  }
+  if(!(flag)){
+    bucket.push(tuple);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage[i];
+  if (!(bucket)){
+    return null;
+  } else if (bucket.length > 0){
+    for(var j = 0; j < bucket.length; j++){
+      if(bucket[j][0] === k){
+        return bucket[j][1];
+      }
+    }
+    return null;
+  } else {
+    return null;
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[i];
+  for (var j = 0 ; j < bucket.length; j++){
+    if(bucket[j][0] === k){
+      bucket.splice(j,1);
+    }
+  }
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ Current these methods have linear time complexity O(n).  Ultimately retrieve 
+ should be constant until we implement doubling and halving.
  */
